21944de1aa639e9149fd6f9a78ea0200b89d6364
Yash Gaira <yash-gaira@users.noreply.github.com>
2026-09-09 02:56:44 +0530

    fix: cache bust PWA icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,17 +11,17 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#0f4b3e',
     icons: [
       {
-        src: '/icon-192x192.png',
+        src: '/icon-192x192.png?v=2',
         sizes: '192x192',
         type: 'image/jpeg',
       },
       {
-        src: '/icon-512x512.png',
+        src: '/icon-512x512.png?v=2',
         sizes: '512x512',
         type: 'image/jpeg',
       },
       {
-        src: '/apple-icon.png',
+        src: '/apple-icon.png?v=2',
         sizes: '180x180',
         type: 'image/jpeg',
       }
