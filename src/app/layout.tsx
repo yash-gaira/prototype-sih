be9270743bdcee81558a6762d55f063e2d4042c1
Yash Gaira <yash-gaira@users.noreply.github.com>
2026-09-09 06:57:11 +0530

    style: remove ViewSwitcher component from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,6 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         {children}
-        <ViewSwitcher />
       </body>
     </html>
   );
